a07156f751948a29682160593c482870b50c7b12
vedha <vedhavijaykumar03@gmail.com>
2026-09-17 03:16:03 +0530

    adding javafx application window

--- src/main/java/com/findash/Main.java
+++ src/main/java/com/findash/Main.java
@@ -1,8 +1,28 @@
 package com.findash;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label title = new Label("FinDash");
+
+        StackPane root = new StackPane(title);
+
+        Scene scene = new Scene(root, 900, 600);
+
+        stage.setTitle("FinDash - Finance Dashboard");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Finance Dashboard");
+        launch(args);
     }
 }
